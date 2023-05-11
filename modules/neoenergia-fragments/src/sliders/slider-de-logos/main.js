ca7d5a5c2f5ecf29e746b6b6be416a316f10e2f0
André Massaki Hirai <56045734+andremhirai@users.noreply.github.com>
2023-05-11 13:49:57 -0300

    slider logos

--- modules/neoenergia-fragments/src/sliders/slider-de-logos/main.js
+++ modules/neoenergia-fragments/src/sliders/slider-de-logos/main.js
@@ -1,4 +1,24 @@
-console.group('Slider de logos');
-console.log('fragmentElement', fragmentElement);
-console.log('configuration', configuration);
-console.groupEnd();
+var swiper = new Swiper('.neo_slider_logos_instance', {
+    navigation: {
+        nextEl: '.neo_slider_logos_instance_actions_btn_next',
+        prevEl: '.neo_slider_logos_instance_actions_btn_prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    slidesPerView: 1.1,
+    spaceBetween: 12,
+    breakpoints: {
+        480: {
+            slidesPerView: 2.2,
+            spaceBetween: 24
+        },
+        768: {
+            slidesPerView: 4.3
+        },
+        1080: {
+            slidesPerView: 6.3
+        }
+
+    }
+});
