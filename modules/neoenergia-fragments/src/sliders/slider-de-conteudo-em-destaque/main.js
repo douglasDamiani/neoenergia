a1e85cc654b8488e9fe2f0117eab91e5d6c6cef6
André Massaki Hirai <56045734+andremhirai@users.noreply.github.com>
2023-05-15 14:37:40 -0300

    fragments

--- modules/neoenergia-fragments/src/sliders/slider-de-conteudo-em-destaque/main.js
+++ modules/neoenergia-fragments/src/sliders/slider-de-conteudo-em-destaque/main.js
@@ -1,21 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var swiper = new Swiper(".neo_swiper_slider_conteudo", {
-    slidesPerView: 1.1,
-    spaceBetween: 24,
-    breakpoints: {
-      480: {
-        slidesPerView: 2.1,
-      },
-      768: {
-        slidesPerView: 3.1,
-      },
-      1080: {
-        slidesPerView: 4,
-      },
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+var swiper = new Swiper('.neo_slider_highlight_content_instance', {
+  navigation: {
+    nextEl: '.neo_slider_highlight_content_instance_actions_btn_next',
+    prevEl: '.neo_slider_highlight_content_instance_actions_btn_prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  slidesPerView: 2.1,
+  spaceBetween: 24,
 });
--- modules/neoenergia-fragments/src/sliders/slider-de-conteudo-sem-imagem/main.js
+++ modules/neoenergia-fragments/src/sliders/slider-de-conteudo-sem-imagem/main.js
@@ -1,4 +1,22 @@
-console.group('Slider de conteudo sem imagem');
-console.log('fragmentElement', fragmentElement);
-console.log('configuration', configuration);
-console.groupEnd();
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".neo_swiper_slider_conteudo", {
+      slidesPerView: 1.1,
+      spaceBetween: 24,
+      breakpoints: {
+        480: {
+          slidesPerView: 2.1,
+        },
+        768: {
+          slidesPerView: 3.1,
+        },
+        1080: {
+          slidesPerView: 4,
+        },
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  });
+  
