d73ff2451ade9d01da8777b7746672ae697ac6d8
ocesar9 <juliocesaoguimaraes@gmail.com>
2023-07-04 16:38:04 -0300

    lfr/feat:NDCTAM2307-2 | Add events-calendar swiper implemetation

--- modules/neoenergia-fragments/src/dynamics/agenda-de-eventos/main.js
+++ modules/neoenergia-fragments/src/dynamics/agenda-de-eventos/main.js
@@ -1,1 +1,19 @@
-console.log("temp12");
+const eventsCalendar = fragmentElement.querySelector(".neoenergia_events-calendar");
+if(eventsCalendar){  
+  const swiper = new Swiper('.neoenergia_events-calendar', {
+    speed: 400,
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        const dates = fragmentElement.querySelectorAll("[data-event-date]");
+        const datesArray = [];
+        dates.forEach(date => {
+          datesArray.push(date.dataset.eventDate);
+        });
+        return '<span class="' + className + '">' + datesArray[index] + '</span>';
+      }
+    }
+  });
+}
